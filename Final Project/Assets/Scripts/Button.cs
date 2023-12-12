92e11408bd2f4f79ba2ec23775e916cb147aee8d
John Heiden <heidenjohn@hotmail.com>
2023-12-12 18:41:58 -0500

    Created New Suns + Randomized Gradients

--- Final Project/Assets/Scripts/Button.cs
+++ Final Project/Assets/Scripts/Button.cs
@@ -15,6 +15,9 @@ public class Button : MonoBehaviour
 
     public bool hasConnections = false;
 
+    [SerializeField]
+    private Gradient[] sunGradients;
+
 	// Start is called before the first frame update
 	void Start()
     {
@@ -36,9 +39,23 @@ public class Button : MonoBehaviour
 
     public void GenerateSystem()
     {
-        for (int i = 0; i < Random.Range(3, 9); i++)
+        GameObject sun = Instantiate(planetPrefabs[planetPrefabs.Length - 1]);
+        sun.GetComponent<Planet>().shapeSettings.planetRadius = Random.Range(8f, 12f);
+		sun.GetComponent<Planet>().shapeSettings.noiseLayers[1].noiseSettings.simpleNoiseSettings.baseRoughness = Random.Range(0.5f, 3f);
+		sun.GetComponent<Planet>().shapeSettings.noiseLayers[1].noiseSettings.simpleNoiseSettings.centre = new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+
+		sun.transform.Rotate(new Vector3(0, Random.Range(0f, 360f), 0));
+
+        int sunGradientIndex = Random.Range(0, sunGradients.Length);
+		sun.GetComponent<Planet>().colourSettings.oceanColour = sunGradients[sunGradientIndex];
+		sun.GetComponent<Planet>().colourSettings.biomeColourSettings.biomes[0].gradient.colorKeys[0] = sunGradients[sunGradientIndex].colorKeys[sunGradients[sunGradientIndex].colorKeys.Length - 1];
+		sun.GetComponent<Planet>().colourSettings.biomeColourSettings.biomes[0].gradient.colorKeys[1] = sunGradients[sunGradientIndex].colorKeys[sunGradients[sunGradientIndex].colorKeys.Length - 1];
+
+		sun.GetComponent<Planet>().GeneratePlanet();
+
+		for (int i = 0; i < Random.Range(3, 9); i++)
         {
-            GameObject planet = Instantiate(planetPrefabs[Random.Range(0, planetPrefabs.Length)]);
+            GameObject planet = Instantiate(planetPrefabs[Random.Range(0, planetPrefabs.Length - 1)]);
             planet.GetComponent<Planet>().shapeSettings.planetRadius = Random.Range(1f, 5f);
 
             planet.GetComponent<Planet>().shapeSettings.noiseLayers[0].noiseSettings.simpleNoiseSettings.baseRoughness = Random.Range(0.2f, 2.3f);
@@ -51,7 +68,7 @@ public class Button : MonoBehaviour
 
             planet.GetComponent<Planet>().GeneratePlanet();
 
-            planet.transform.position = new Vector3(10 * i, 0, 0);
+            planet.transform.position = new Vector3(sun.GetComponent<Planet>().shapeSettings.planetRadius + 10 * (i + 1), 0, 0);
             planet.transform.Rotate(new Vector3(0, Random.Range(0f, 360f), 0));
         }
 	}
